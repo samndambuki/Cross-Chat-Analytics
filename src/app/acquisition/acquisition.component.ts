fbf0d03d41d4ed384519155e52b2448060eb9afb
samndambuki <sam.ndambuki08@gmail.com>
2023-09-27 06:12:08 +0300

    worked on acquisition component

--- src/app/acquisition/acquisition.component.ts
+++ src/app/acquisition/acquisition.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-acquisition',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './acquisition.component.html',
-  styleUrls: ['./acquisition.component.css']
+  styleUrls: ['./acquisition.component.css'],
 })
 export class AcquisitionComponent {
-
+  faUser = faUserCircle;
 }
